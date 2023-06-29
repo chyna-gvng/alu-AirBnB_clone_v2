18846a3f189f2d6a80115bf861b64444e8c1c7b0
Walter <w.angoye@alustudent.com>
2023-06-29 21:35:36 +0300

    Updated 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -6,15 +6,13 @@
 from models import storage
 from models.state import State
 from flask import Flask, render_template
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
 @app.route('/cities_by_states')
 def states_list():
     """
-        Return: HTML page with a list of states
+        Return: HTML page with list of states
     """
     path = '8-cities_by_states.html'
     states = storage.all(State)
@@ -22,12 +20,13 @@ def states_list():
 
 
 @app.teardown_appcontext
-def app_teardown(exception=None):
+def app_teardown(arg=None):
     """
-        Clean up the session
+        Clean-up session
     """
     storage.close()
 
 
 if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
