abae0143b951742ca76b4c581c94cb26328cab2d
Walter <w.angoye@alustudent.com>
2023-06-29 22:11:29 +0300

    Updated 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
-"""
-    python script that starts a Flask web application
-"""
+"""Web App"""
 
 from models import storage
 from models.state import State
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/states')
-@app.route('/states/<id>')
-def states_list(id=None):
-    """
-        Return: HTML page with list of states
-    """
-    path = '9-states.html'
-    states = storage.all('State')
-    return render_template(path, states=states, id=id)
+@app.route('/states', strict_slashes=False)
+def states_list():
+    """Return HTML page with a list of states"""
+    states = storage.all(State).values()
+    return render_template('9-states.html', states=states, id=None)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id(id):
+    """Return HTML page with cities of a specific state"""
+    states = storage.all(State)
+    key = "State." + id
+    state = states.get(key)
+    if state is None:
+        return render_template('9-states.html', id=None)
+    else:
+        return render_template('9-states.html', state=state, id=id)
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """
-        Clean-up session
-    """
+def teardown_appcontext(exception):
+    """Close the current SQLAlchemy session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
