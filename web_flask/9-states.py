ac89a46f2cf73a8090888e4d54489afdedb42cc0
Walter <w.angoye@alustudent.com>
2023-06-29 22:39:32 +0300

    Updated 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,33 +1,37 @@
 #!/usr/bin/python3
 """
-    python script that starts a Flask web application
+Starts a Flask web application
 """
 
-from models import storage
-from models.state import State
+from http.client import FOUND
 from flask import Flask, render_template
+from models import *
+from models.state import State
 app = Flask(__name__)
 
 
-@app.route('/states')
-@app.route('/states/<id>')
-def states_list(id=None):
-    """
-        Return: HTML page with list of states
-    """
-    path = '9-states.html'
+@app.route('/states', strict_slashes=False)
+def states():
+    """states"""
     states = storage.all('State')
-    return render_template(path, states=states, id=id)
+    return render_template('9-states.html', states=states, id=False)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """states with filter (Objects State)"""
+    states = storage.all('State')
+    for state in states.values():
+        if state.id == str(id):
+            return render_template('9-states.html', states=state)
+    return render_template('9-states.html', no_found=True)
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """
-        Clean-up session
-    """
+def storage_close(self):
+    '''Datasource'''
     storage.close()
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000,)
