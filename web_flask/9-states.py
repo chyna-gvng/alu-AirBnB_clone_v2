55715d7fe33a1875fb3c0723733ecad1c8a3eb08
Walter <w.angoye@alustudent.com>
2023-06-29 22:57:20 +0300

    Updated 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/states')
 @app.route('/states/<id>')
-def states_list(id=True):
+def states_list(id=None):
     """
         Return: HTML page with list of states
     """
