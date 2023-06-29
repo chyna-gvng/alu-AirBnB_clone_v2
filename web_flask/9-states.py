2ed1a63dedf88d8e1dfe21688192951485c12d9d
Walter <w.angoye@alustudent.com>
2023-06-29 22:28:47 +0300

    Updated 9-states.py

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -4,30 +4,39 @@
 """
 
 from models import storage
-from models.state import State
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/states')
-@app.route('/states/<id>')
-def states_list(id=None):
+@app.route('/states', strict_slashes=False)
+def states_list():
     """
-        Return: HTML page with list of states
+    Display a HTML page with the list of states
     """
-    path = '9-states.html'
-    states = storage.all('State')
-    return render_template(path, states=states, id=id)
+    states = storage.all('State').values()
+    return render_template('9-states.html', states=states)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_by_id(id):
+    """
+    Display a HTML page with the list of cities for a state
+    """
+    state = storage.get('State', id)
+    if state:
+        return render_template('9-states.html', state=state)
+    else:
+        return render_template('9-states.html', not_found=True)
 
 
 @app.teardown_appcontext
 def app_teardown(arg=None):
     """
-        Clean-up session
+    Clean-up session
     """
     storage.close()
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
