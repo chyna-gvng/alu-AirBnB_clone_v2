74ddcf5c42d9db3e8a5c43e35250b2c3314e42e6
Walter <w.angoye@alustudent.com>
2023-06-29 20:58:36 +0300

    Updated 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,40 +1,27 @@
 #!/usr/bin/python3
-"""
-    python script that starts a Flask web application
-"""
+
+"""Starts a Flask web application"""
 
 from models import storage
 from models.state import State
-from flask import Flask, render_template, request, jsonify
-from os import getenv
-
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def states_list():
-    """
-        Return: HTML page with list of states
-    """
-    path = '7-states_list.html'
-    states = storage.all(State)
-    """
-        sort State object alphabetically by name
-    """
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, sorted_states=sorted_states)
+def states():
+    """Comment"""
+    return render_template('7-states_list.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """
-        Clean-up session
-    """
+def teardown(self):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    host = getenv("HBNB_API_HOST", "0.0.0.0")
-    port = getenv("HBNB_API_PORT", "5000")
-    app.run(host=host, port=port, threaded=True)
+    app.run(host='0.0.0.0')
