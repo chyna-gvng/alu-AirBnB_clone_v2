7f61a62b9d5d74edd5ebe7d70b91d27230b92844
Walter <w.angoye@alustudent.com>
2023-06-29 21:09:12 +0300

    Updated 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -5,28 +5,24 @@
 
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states')
-def states_list():
-    """
-        Return: HTML page with list of states
-    """
-    path = '8-cities_by_states.html'
-    states = storage.all(State)
-    return render_template(path, states=states)
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_route():
+    """Comment"""
+    return render_template('8-cities_by_states.html',
+                           states=storage.all('State').values())
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """
-        Clean-up session
-    """
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
